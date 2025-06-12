3f9cc4590f6062084a7aca2adc6f2636bd4fb62b
BlackiePearlJoobi <katze526reiswein@gmail.com>
2025-06-11 20:19:20 -0500

    Fixed layout issues for small screens

--- src/components/InGame.tsx
+++ src/components/InGame.tsx
@@ -388,8 +388,7 @@ const InGame = () => {
       // (Medium) default: pick a random column but prefer center
       if (opponent === "CPU_medium" && !targetCol) {
         const centerdArr = [
-          1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6,
-          6, 7,
+          1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 7,
         ];
         do {
           const index = getRandomInt(0, centerdArr.length - 1);
